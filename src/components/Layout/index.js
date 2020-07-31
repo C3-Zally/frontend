4642fc874d605fa41a33331c0def84fcf8c46b90
Jorge Ossa <jorgehossa@gmail.com>
2020-07-31 00:56:50 -0500

    Feature: Add base layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Layout.scss';
+import { NavBar } from '../NavBar';
 
 const Layout = ({children}) => {
   return (
-    <main className='app'>
-      {children}
+    <main className='Layout__wrapper'>
+      <NavBar />
+      <section className='Layout__content'>
+        {children}
+      </section>
     </main>
   );
 }
