26396dcd888f6e3eba32eef10a942e94a6b6b0d1
Jorge Ossa <jorgehossa@gmail.com>
2020-08-02 00:13:16 -0500

    Refactor NavBar component
    - Add links info via component props and extract link structure to render links in a map function.
    - Change logo structure to take a better handle of state change.

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,50 +2,43 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
 import './NavBar.scss';
-import logoFull from '../../assets/logo.svg';
 
-export const NavBar = () => {
-  const [expanded, setExpanded] = useState(true);
+const NavBar = ({ links }) => {
+  const [expanded, setExpanded] = useState(false);
 
-  const handleExpantion = () => {
+  const NavBarItem = ({ item }) => (
+    <li className='NavBar__item'>
+      <Link className='NavBar__link' to={item.to}>
+        <i className={item.iconClass}></i>
+        {expanded && <span>{item.label}</span>}
+      </Link>
+    </li>
+  );
+
+  const handleExpantion = (e) => {
     setExpanded(!expanded);
-  }
+  };
 
   const NavBarClasses = classNames('NavBar', {
-    'isContracted': !expanded,
-  })
+    isContracted: !expanded,
+  });
 
   return (
     <nav className={NavBarClasses}>
-      <div className="NavBar__trigger" onClick={handleExpantion}>
-        {expanded ?
-          <i className="icon__arrow-narrow--left"></i> :
-          <i className="icon__arrow-narrow--right"></i>
-        }
+      <div className='NavBar__trigger' onClick={handleExpantion}>
+        {expanded ? (
+          <i className='icon__arrow-narrow--left'></i>
+        ) : (
+          <i className='icon__arrow-narrow--right'></i>
+        )}
       </div>
-        <div className='NavBar__logo'>
-          {expanded ? <img src={logoFull} alt='Hope Tracker' /> : <i className='icon__brand'></i> }
-        </div>
-        <ul className='NavBar__list'>
-          <li className="NavBar__item">
-            <Link className="NavBar__link" to='/'>
-              <i className='icon__home'></i>
-              {expanded && <span>Home</span>}
-            </Link>
-          </li>
-          <li className="NavBar__item">
-            <Link className="NavBar__link" to='/global'>
-              <i className='icon__world'></i>
-              {expanded && <span>Global</span>}
-            </Link>
-          </li>
-          <li className="NavBar__item">
-            <Link className="NavBar__link" to='/take-care'>
-              <i className='icon__medical-cross'></i>
-              {expanded && <span>Take Care</span>}
-            </Link>
-          </li>
-        </ul>
+      <div className='NavBar__logo'></div>
+      <ul className='NavBar__list'>
+        {links.map((link) => (
+          <NavBarItem item={link} key={link.label} />
+        ))}
+      </ul>
     </nav>
-  )
-}
+  );
+};
+export default NavBar;
