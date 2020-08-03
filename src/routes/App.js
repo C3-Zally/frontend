1709d55dabfff6ea5bf84e3f2fb8a1c2abe9520b
Jorge Ossa <jorgehossa@gmail.com>
2020-08-03 12:42:08 -0500

    Fix prettier pre-commit config

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "../containers/Home";
-import Global from "../containers/Global";
-import Country from "../containers/Country";
-import TakeCare from "../containers/TakeCare";
-import Error404 from "../containers/Error404";
-import Layout from "../components/Layout";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from '../containers/Home';
+import Global from '../containers/Global';
+import Country from '../containers/Country';
+import TakeCare from '../containers/TakeCare';
+import Error404 from '../containers/Error404';
+import Layout from '../components/Layout';
 const App = () => (
   <Router>
     <Layout>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/global" component={Global} />
-        <Route exact path="/country/:id" component={Country} />
-        <Route exact path="/take-care" component={TakeCare} />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/global' component={Global} />
+        <Route exact path='/country/:id' component={Country} />
+        <Route exact path='/take-care' component={TakeCare} />
         <Route component={Error404} />
       </Switch>
     </Layout>
