da6bb93ced00ac5d767d006da2a79366c9ca2692
Alejandro Medina <5almero5@gmail.com>
2020-08-03 20:10:16 -0500

    Refactor sarch bar location

---
+++ src/components/SearchCountryBar/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SearchCountryBar = () => {
+  return (
+    <div>
+      <input type='text' className='input' placeholder='Search...' />
+    </div>
+  );
+};
+export default SearchCountryBar;
