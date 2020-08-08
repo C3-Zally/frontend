0902e82f20b44609c7272662c660a20fc42706f5
ingeniero.miguelvargas@gmail.com <ingeniero.miguelvargas@gmail.com>
2020-08-08 18:11:24 -0500

    FIX Config Deploy

--- backend/settings.py
+++ backend/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Import django_heroku module at the top of your settings.py file
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
