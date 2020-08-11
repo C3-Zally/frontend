11dffdc0d53e6432dea2e1979bc4eb04d965feae
Natanael León <nj.leon.15@gmail.com>
2020-08-11 13:00:30 -0500

    Fix: Responsive Issue, falta acoplar toda la sección de de takecare a vw

--- src/components/CareCard/index.js
+++ src/components/CareCard/index.js
@@ -5,7 +5,7 @@ const CareCard = ({ title, image }) => (
   <React.Fragment>
     <div className='CareCard__item'>
       <img src={image} alt='' />
-      <h4>{title}</h4>
+      <span className='CareCard__item--title'>{title}</span>
     </div>
   </React.Fragment>
 );
