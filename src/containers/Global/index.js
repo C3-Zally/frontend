7c4cb7809439b6d3244b5543f066bcc6fc833423
Alejandro Medina <5almero5@gmail.com>
2020-08-14 12:33:48 -0500

    Add global components

--- src/containers/Global/index.js
+++ src/containers/Global/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import GlobalMap from '../../components/GlobalMap';
 
+import MainBanner from '../../components/MainBanner';
+import TitleSection from '../../components/TitleSection';
+import ProgressChart from '../../components/ProgressChart';
+import GlobalMap from '../../components/GlobalMap';
+import DonutChart from '../../components/DonutChart';
 const Global = (params) => {
   return (
     <>
-      <h1>Global</h1>
-      <GlobalMap />
+      <section className='overview'>
+        <TitleSection title='Overview' />
+        <ProgressChart />
+      </section>
+      <section className='rates'>
+        <DonutChart type='isRecovery' />
+        <DonutChart type='isFatality' />
+      </section>
+      <section className='progressChart'>
+        <TitleSection title='New Cases' infoButton />
+        <GlobalMap />
+      </section>
     </>
   );
 };
