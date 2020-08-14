7a5f2e52c5c8aea806319c7f01dbeea73cb47ba0
Alejandro Medina <5almero5@gmail.com>
2020-08-13 21:06:37 -0500

    Add map and filter map example

--- src/components/GlobalMap/index.js
+++ src/components/GlobalMap/index.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
 import './GlobalMap.scss';
-
-const CountryMap = (props) => {
+const GlobalMap = (props) => {
   const geoUrl =
     'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json';
 
   return (
     <>
-      <div>
-        <ComposableMap>
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography key={geo.rsmKey} geography={geo} />
-              ))
-            }
-          </Geographies>
-        </ComposableMap>
-      </div>
+      <ComposableMap className='map'>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) => {
+            return geographies.map(
+              (geo) =>
+                geo.properties.ISO_A2 === 'BR' && (
+                  <Geography key={geo.rsmKey} geography={geo} />
+                )
+            );
+          }}
+        </Geographies>
+      </ComposableMap>
     </>
   );
 };
 
-export default CountryMap;
+export default GlobalMap;
