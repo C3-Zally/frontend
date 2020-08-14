d2fb5545280fffe23bd1898b66e7637aabe550ea
Alejandro Medina <5almero5@gmail.com>
2020-08-14 11:58:35 -0500

    Add haigh light country on map

--- src/components/GlobalMap/index.js
+++ src/components/GlobalMap/index.js
@@ -4,27 +4,21 @@ import './GlobalMap.scss';
 const GlobalMap = ({ alpha2Code }) => {
   const geoUrl =
     'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json';
-
-  const globalMap = (item) => <Geography key={item.rsmKey} geography={item} />;
-
   const filterMap = (item) => {
-    if (item.properties.ISO_A2 === alpha2Code) {
-      return (
-        <Geography key={item.rsmKey} geography={item} className='globalMap' />
-      );
+    if (alpha2Code) {
+      if (item.properties.ISO_A2 === alpha2Code) {
+        return (
+          <Geography key={item.rsmKey} geography={item} className='globalMap' />
+        );
+      }
     }
+    return <Geography key={item.rsmKey} geography={item} />;
   };
   return (
     <>
       <ComposableMap className='map'>
         <Geographies geography={geoUrl}>
-          {({ geographies }) => {
-            if (alpha2Code) {
-              return geographies.map(filterMap);
-            } else {
-              return geographies.map(globalMap);
-            }
-          }}
+          {({ geographies }) => geographies.map(filterMap)}
         </Geographies>
       </ComposableMap>
     </>
